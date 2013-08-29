ae3b18c5be35a8e4f869154b062684ce3e8d7b68
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2013-08-29 16:26:21 -0400

    stream: rename buffer to get and set

--- include/stream.h
+++ include/stream.h
@@ -25,19 +25,19 @@ public:
         return (*this);
     }
 
-    stream& operator<<(const stream& value) { return (*this) << value.buffer(); }
+    stream& operator<<(const stream& value) { return (*this) << value.get(); }
 
-    std::string buffer() const { return str(); }
-    void buffer(const std::string& value) { str(value); }
+    std::string get() const { return str(); }
+    void set(const std::string& value) { str(value); }
 
-    operator std::string() const { return buffer(); }
-    const char* data() const { return buffer().data(); }
+    operator std::string() const { return get(); }
+    const char* data() const { return get().data(); }
 
-    void clear() { buffer(std::string()); }
+    void clear() { set(std::string()); }
 
 #ifdef QT_CORE_LIB
     stream& operator<<(const QString& value) { return (*this) << value.toStdString(); }
-    operator QString() { return QString::fromStdString(buffer()); }
+    operator QString() { return QString::fromStdString(get()); }
 #endif
 };
 
