6af1188c583a2b48b58b9958d36602826f9bcd0b
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2013-10-08 16:03:15 -0400

    stream: simplify
    Replace stream class with << operator for strings.

--- stream.h
+++ stream.h
@@ -10,43 +10,24 @@
 #define STREAM_H
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-#include <sstream>
-
-#ifdef QT_CORE_LIB
-#  include <QString>
-#endif
+#include "convert.h"
+#include <string>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-class stream: public std::ostringstream
+template<typename T>
+std::string& operator<<(std::string& stream, const T& value)
 {
-public:
-    stream() = default;
+    stream+= convert::to(value);
+    return stream;
+}
 
-    template<typename T>
-    explicit stream(const T& value) { (*this) << value; }
-
-    template<typename T>
-    stream& operator<<(const T& value)
-    {
-        *static_cast<std::ostringstream*>(this) << value;
-        return (*this);
-    }
-
-    stream& operator<<(const stream& value) { return (*this) << value.get(); }
-
-    std::string get() const { return str(); }
-    void set(const std::string& value) { str(value); }
-
-    operator std::string() const { return get(); }
-    const char* data() const { return get().data(); }
-
-    void clear() { set(std::string()); }
-
-#ifdef QT_CORE_LIB
-    stream& operator<<(const QString& value) { return (*this) << value.toStdString(); }
-    operator QString() { return QString::fromStdString(get()); }
-#endif
-};
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename T>
+std::string operator<<(std::string&& stream, const T& value)
+{
+    stream+= convert::to(value);
+    return stream;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #endif
