ff196ca81768311776d58efd3fc0b09601c0d469
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2013-10-09 20:20:29 -0400

    slap: rename slap_error to errc
    Make is enum class. Get error codes from ldap.h instead of error.c and
    add user_error.
    Add constructors that accept hei::errc.

--- slap/slap_error.h
+++ slap/slap_error.h
@@ -21,102 +21,108 @@ namespace slap
 {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-enum class slap_error
+enum class errc
 {
-    ldap_success                    = LDAP_SUCCESS,
-    ldap_operations_error           = LDAP_OPERATIONS_ERROR,
-    ldap_protocol_error             = LDAP_PROTOCOL_ERROR,
-    ldap_timelimit_exceeded         = LDAP_TIMELIMIT_EXCEEDED,
-    ldap_sizelimit_exceeded         = LDAP_SIZELIMIT_EXCEEDED,
-    ldap_compare_false              = LDAP_COMPARE_FALSE,
-    ldap_compare_true               = LDAP_COMPARE_TRUE,
-    ldap_strong_auth_not_supported  = LDAP_STRONG_AUTH_NOT_SUPPORTED,
-    ldap_strong_auth_required       = LDAP_STRONG_AUTH_REQUIRED,
+    success                        = LDAP_SUCCESS,
 
-    ldap_referral                   = LDAP_REFERRAL,
-    ldap_adminlimit_exceeded        = LDAP_ADMINLIMIT_EXCEEDED,
-    ldap_unavailable_critical_extension= LDAP_UNAVAILABLE_CRITICAL_EXTENSION,
-    ldap_confidentiality_required   = LDAP_CONFIDENTIALITY_REQUIRED,
-    ldap_sasl_bind_in_progress      = LDAP_SASL_BIND_IN_PROGRESS,
+    operations_error               = LDAP_OPERATIONS_ERROR,
+    protocol_error                 = LDAP_PROTOCOL_ERROR,
+    timelimit_exceeded             = LDAP_TIMELIMIT_EXCEEDED,
+    sizelimit_exceeded             = LDAP_SIZELIMIT_EXCEEDED,
+    compare_false                  = LDAP_COMPARE_FALSE,
+    compare_true                   = LDAP_COMPARE_TRUE,
+    auth_method_not_supported      = LDAP_AUTH_METHOD_NOT_SUPPORTED,
+    strong_auth_not_supported      = LDAP_STRONG_AUTH_NOT_SUPPORTED,
+    strong_auth_required           = LDAP_STRONG_AUTH_REQUIRED,
+    stronger_auth_required         = LDAP_STRONGER_AUTH_REQUIRED,
+    partial_results                = LDAP_PARTIAL_RESULTS,
 
-    ldap_no_such_attribute          = LDAP_NO_SUCH_ATTRIBUTE,
-    ldap_undefined_type             = LDAP_UNDEFINED_TYPE,
-    ldap_inappropriate_matching     = LDAP_INAPPROPRIATE_MATCHING,
-    ldap_constraint_violation       = LDAP_CONSTRAINT_VIOLATION,
-    ldap_type_or_value_exists       = LDAP_TYPE_OR_VALUE_EXISTS,
-    ldap_invalid_syntax             = LDAP_INVALID_SYNTAX,
+    referral                       = LDAP_REFERRAL,
+    adminlimit_exceeded            = LDAP_ADMINLIMIT_EXCEEDED,
+    unavailable_critical_extension = LDAP_UNAVAILABLE_CRITICAL_EXTENSION,
+    confidentiality_required       = LDAP_CONFIDENTIALITY_REQUIRED,
+    sasl_bind_in_progress          = LDAP_SASL_BIND_IN_PROGRESS,
 
-    ldap_no_such_object             = LDAP_NO_SUCH_OBJECT,
-    ldap_alias_problem              = LDAP_ALIAS_PROBLEM,
-    ldap_invalid_dn_syntax          = LDAP_INVALID_DN_SYNTAX,
+    no_such_attribute              = LDAP_NO_SUCH_ATTRIBUTE,
+    undefined_type                 = LDAP_UNDEFINED_TYPE,
+    inappropriate_matching         = LDAP_INAPPROPRIATE_MATCHING,
+    constraint_violation           = LDAP_CONSTRAINT_VIOLATION,
+    type_or_value_exists           = LDAP_TYPE_OR_VALUE_EXISTS,
+    invalid_syntax                 = LDAP_INVALID_SYNTAX,
 
-    ldap_alias_deref_problem        = LDAP_ALIAS_DEREF_PROBLEM,
+    no_such_object                 = LDAP_NO_SUCH_OBJECT,
+    alias_problem                  = LDAP_ALIAS_PROBLEM,
+    invalid_dn_syntax              = LDAP_INVALID_DN_SYNTAX,
+    is_leaf                        = LDAP_IS_LEAF,
+    alias_deref_problem            = LDAP_ALIAS_DEREF_PROBLEM,
 
-    ldap_inappropriate_auth         = LDAP_INAPPROPRIATE_AUTH,
-    ldap_invalid_credentials        = LDAP_INVALID_CREDENTIALS,
-    ldap_insufficient_access        = LDAP_INSUFFICIENT_ACCESS,
-    ldap_busy                       = LDAP_BUSY,
-    ldap_unavailable                = LDAP_UNAVAILABLE,
-    ldap_unwilling_to_perform       = LDAP_UNWILLING_TO_PERFORM,
-    ldap_loop_detect                = LDAP_LOOP_DETECT,
+    proxy_authz_failure            = LDAP_X_PROXY_AUTHZ_FAILURE,
+    inappropriate_auth             = LDAP_INAPPROPRIATE_AUTH,
+    invalid_credentials            = LDAP_INVALID_CREDENTIALS,
+    insufficient_access            = LDAP_INSUFFICIENT_ACCESS,
 
-    ldap_naming_violation           = LDAP_NAMING_VIOLATION,
-    ldap_object_class_violation     = LDAP_OBJECT_CLASS_VIOLATION,
-    ldap_not_allowed_on_nonleaf     = LDAP_NOT_ALLOWED_ON_NONLEAF,
-    ldap_not_allowed_on_rdn         = LDAP_NOT_ALLOWED_ON_RDN,
-    ldap_already_exists             = LDAP_ALREADY_EXISTS,
-    ldap_no_object_class_mods       = LDAP_NO_OBJECT_CLASS_MODS,
+    busy                           = LDAP_BUSY,
+    unavailable                    = LDAP_UNAVAILABLE,
+    unwilling_to_perform           = LDAP_UNWILLING_TO_PERFORM,
+    loop_detect                    = LDAP_LOOP_DETECT,
 
-    ldap_affects_multiple_dsas      = LDAP_AFFECTS_MULTIPLE_DSAS,
+    naming_violation               = LDAP_NAMING_VIOLATION,
+    object_class_violation         = LDAP_OBJECT_CLASS_VIOLATION,
+    not_allowed_on_nonleaf         = LDAP_NOT_ALLOWED_ON_NONLEAF,
+    not_allowed_on_rdn             = LDAP_NOT_ALLOWED_ON_RDN,
+    already_exists                 = LDAP_ALREADY_EXISTS,
+    no_object_class_mods           = LDAP_NO_OBJECT_CLASS_MODS,
+    results_too_large              = LDAP_RESULTS_TOO_LARGE,
+    affects_multiple_dsas          = LDAP_AFFECTS_MULTIPLE_DSAS,
 
-    ldap_vlv_error                  = LDAP_VLV_ERROR,
+    vlv_error                      = LDAP_VLV_ERROR,
 
-    ldap_other                      = LDAP_OTHER,
+    other                          = LDAP_OTHER,
 
-    ldap_partial_results            = LDAP_PARTIAL_RESULTS,
-    ldap_is_leaf                    = LDAP_IS_LEAF,
+    cup_resources_exhausted        = LDAP_CUP_RESOURCES_EXHAUSTED,
+    cup_security_violation         = LDAP_CUP_SECURITY_VIOLATION,
+    cup_invalid_data               = LDAP_CUP_INVALID_DATA,
+    cup_unsupported_scheme         = LDAP_CUP_UNSUPPORTED_SCHEME,
+    cup_reload_required            = LDAP_CUP_RELOAD_REQUIRED,
 
-    ldap_results_too_large          = LDAP_RESULTS_TOO_LARGE,
+    cancelled                      = LDAP_CANCELLED,
+    no_such_operation              = LDAP_NO_SUCH_OPERATION,
+    too_late                       = LDAP_TOO_LATE,
+    cannot_cancel                  = LDAP_CANNOT_CANCEL,
 
-    ldap_cancelled                  = LDAP_CANCELLED,
-    ldap_no_such_operation          = LDAP_NO_SUCH_OPERATION,
-    ldap_too_late                   = LDAP_TOO_LATE,
-    ldap_cannot_cancel              = LDAP_CANNOT_CANCEL,
+    assertion_failed               = LDAP_ASSERTION_FAILED,
 
-    ldap_assertion_failed           = LDAP_ASSERTION_FAILED,
-    ldap_x_assertion_failed         = LDAP_X_ASSERTION_FAILED,
+    proxied_authorization_denied   = LDAP_PROXIED_AUTHORIZATION_DENIED,
 
-    ldap_proxied_authorization_denied= LDAP_PROXIED_AUTHORIZATION_DENIED,
-    ldap_x_proxy_authz_failure      = LDAP_X_PROXY_AUTHZ_FAILURE,
+    sync_refresh_required          = LDAP_SYNC_REFRESH_REQUIRED,
 
-    ldap_sync_refresh_required      = LDAP_SYNC_REFRESH_REQUIRED,
-    ldap_x_sync_refresh_required    = LDAP_X_SYNC_REFRESH_REQUIRED,
+    no_operation                   = LDAP_X_NO_OPERATION,
 
-    ldap_x_no_operation             = LDAP_X_NO_OPERATION,
+    no_referrals_found             = LDAP_X_NO_REFERRALS_FOUND,
+    cannot_chain                   = LDAP_X_CANNOT_CHAIN,
 
-    ldap_cup_resources_exhausted    = LDAP_CUP_RESOURCES_EXHAUSTED,
-    ldap_cup_security_violation     = LDAP_CUP_SECURITY_VIOLATION,
-    ldap_cup_invalid_data           = LDAP_CUP_INVALID_DATA,
-    ldap_cup_unsupported_scheme     = LDAP_CUP_UNSUPPORTED_SCHEME,
-    ldap_cup_reload_required        = LDAP_CUP_RELOAD_REQUIRED,
+    invalidreference               = LDAP_X_INVALIDREFERENCE,
 
-    ldap_server_down                = LDAP_SERVER_DOWN,
-    ldap_local_error                = LDAP_LOCAL_ERROR,
-    ldap_encoding_error             = LDAP_ENCODING_ERROR,
-    ldap_decoding_error             = LDAP_DECODING_ERROR,
-    ldap_timeout                    = LDAP_TIMEOUT,
-    ldap_auth_unknown               = LDAP_AUTH_UNKNOWN,
-    ldap_filter_error               = LDAP_FILTER_ERROR,
-    ldap_user_cancelled             = LDAP_USER_CANCELLED,
-    ldap_param_error                = LDAP_PARAM_ERROR,
-    ldap_no_memory                  = LDAP_NO_MEMORY,
-    ldap_connect_error              = LDAP_CONNECT_ERROR,
-    ldap_not_supported              = LDAP_NOT_SUPPORTED,
-    ldap_control_not_found          = LDAP_CONTROL_NOT_FOUND,
-    ldap_no_results_returned        = LDAP_NO_RESULTS_RETURNED,
-    ldap_more_results_to_return     = LDAP_MORE_RESULTS_TO_RETURN,
-    ldap_client_loop                = LDAP_CLIENT_LOOP,
-    ldap_referral_limit_exceeded    = LDAP_REFERRAL_LIMIT_EXCEEDED
+    server_down                    = LDAP_SERVER_DOWN,
+    local_error                    = LDAP_LOCAL_ERROR,
+    encoding_error                 = LDAP_ENCODING_ERROR,
+    decoding_error                 = LDAP_DECODING_ERROR,
+    timeout                        = LDAP_TIMEOUT,
+    auth_unknown                   = LDAP_AUTH_UNKNOWN,
+    filter_error                   = LDAP_FILTER_ERROR,
+    user_cancelled                 = LDAP_USER_CANCELLED,
+    param_error                    = LDAP_PARAM_ERROR,
+    no_memory                      = LDAP_NO_MEMORY,
+    connect_error                  = LDAP_CONNECT_ERROR,
+    not_supported                  = LDAP_NOT_SUPPORTED,
+    control_not_found              = LDAP_CONTROL_NOT_FOUND,
+    no_results_returned            = LDAP_NO_RESULTS_RETURNED,
+    more_results_to_return         = LDAP_MORE_RESULTS_TO_RETURN,
+    client_loop                    = LDAP_CLIENT_LOOP,
+    referral_limit_exceeded        = LDAP_REFERRAL_LIMIT_EXCEEDED,
+    connecting                     = LDAP_X_CONNECTING,
+
+    user_error                     = 0x7fff /* hopefully this is not used by LDAP */
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -124,16 +130,16 @@ class slap_category: public std::error_category
 {
 public:
     const char* name() const noexcept override { return "ldap"; }
-    std::string message(int ev) const override { return std::string(ldap_err2string(ev)); }
+    std::string message(int ev) const override { return ev==int(errc::user_error)? std::string(): ldap_err2string(ev); }
 };
 
 const std::error_category& slap_category();
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-inline std::error_code make_error_code(slap_error e)
+inline std::error_code make_error_code(slap::errc e)
 { return std::error_code(int(e), slap_category()); }
 
-inline std::error_condition make_error_condition(slap_error e)
+inline std::error_condition make_error_condition(slap::errc e)
 { return std::error_condition(int(e), slap_category()); }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -144,7 +150,7 @@ inline std::error_condition make_error_condition(slap_error e)
 namespace std
 {
     template<>
-    struct is_error_code_enum<slap::slap_error>: public true_type { };
+    struct is_error_code_enum<slap::errc>: public true_type { };
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -160,8 +166,16 @@ public:
         exception(message, std::error_code(error, slap::slap_category()))
     { }
 
+    slap_exception(slap::errc error, const std::string& message= std::string()):
+        exception(message, std::error_code(int(error), slap::slap_category()))
+    { }
+
     slap_exception(const std::string& file, const std::string& func, int line, int error, const std::string& message= std::string()):
         exception(file, func, line, message, std::error_code(error, slap::slap_category()))
+    { }
+
+    slap_exception(const std::string& file, const std::string& func, int line, slap::errc error, const std::string& message= std::string()):
+        exception(file, func, line, message, std::error_code(int(error), slap::slap_category()))
     { }
 };
 
