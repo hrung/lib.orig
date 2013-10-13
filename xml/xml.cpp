6c15e86af408823505ba5270704f9b2514a61fc4
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2013-10-12 22:40:17 -0400

    Add xml classes
    This is an updated version of the original xml classes.

---
+++ xml/xml.cpp
@@ -0,0 +1,279 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#include "xml.h"
+
+#include "stream.h"
+#include "except.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+namespace xml
+{
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+const std::error_category& xml_category()
+{
+    static class xml_category instance;
+    return instance;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void validate_name(const std::string& name)
+{
+    auto ri= name.begin();
+    if( ri==name.end() || !(isalpha(*ri) || *ri==':' || *ri=='_') ) throw xml_except(errc::syntax);
+
+    for(++ri; ri!=name.end(); ++ri)
+        if( !(isalnum(*ri) || *ri==':' || *ri=='_' || *ri=='.' || *ri=='-') )
+    throw xml_except(errc::syntax);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void validate_value(const std::string& value)
+{
+    if(std::string::npos != value.find_first_of(R"('")")) throw xml_except(errc::syntax);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void validate_content(const std::string& value)
+{
+    if(std::string::npos && value.find_first_of("<>")) throw xml_except(errc::syntax);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+bool parse_name(std::string& source, std::string& name)
+{
+    auto ri= source.begin();
+    if( !(isalpha(*ri) || *ri==':' || *ri=='_') ) return false;
+
+    for(++ri; ri!=source.end(); ++ri)
+        if( !(isalnum(*ri) || *ri==':' || *ri=='_' || *ri=='.' || *ri=='-') )
+    break;
+
+    name.assign(source.begin(), ri); source.erase(source.begin(), ri);
+    return name.size();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void parse_space(std::string& source)
+{
+    for(auto ri= source.begin(); ri!=source.end(); ++ri)
+        if(!isspace(*ri))
+        {
+            source.erase(source.begin(), ri);
+            break;
+        }
+}
+
+inline void chop(std::string& source) { source.erase(0, 1); }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+std::string attribute::_M_write() const
+{
+    if(_M_name.empty())
+        return std::string();
+    else return " "+ _M_name+ "=\""+ _M_value+ "\"";
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void attribute::_M_parse(std::string& source)
+{
+    std::string name;
+    if(!parse_name(source, name)) return;
+    _M_name.assign(name);
+
+    parse_space(source);
+    if(source[0] != '=') throw xml_except(errc::invalid_token);
+    chop(source);
+
+    parse_space(source);
+    char quote= source[0];
+    if( !(quote=='"' || quote=='\'') ) throw xml_except(errc::invalid_token);
+    chop(source);
+
+    size_t pos= source.find(quote);
+    if(pos==std::string::npos) throw xml_except(errc::invalid_token);
+    _M_value.assign(source, 0, pos);
+    source.erase(0, ++pos);
+
+    parse_space(source);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+inline std::string decorate(const std::string& value, bool nice, int ix)
+{
+    return nice? std::string(2*ix, ' ')+ value+ '\n': value;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+std::string tag::_M_write(tag_type type, bool nice, int ix) const
+{
+    std::string value("<");
+        if(type==end_tag) value+= "/";
+    value+= _M_name;
+
+    if(type==start_tag || type==empty_tag)
+        for(auto ri= _M_attributes.begin(); ri != _M_attributes.end(); ++ri)
+    value+= ri->_M_write();
+
+    if(type==empty_tag) value+= "/";
+    value+= ">";
+
+    return decorate(value, nice, ix);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void tag::_M_parse(std::string& source, tag_type type, bool& empty)
+{
+    if(source[0] != '<') throw xml_except(errc::invalid_token);
+    chop(source);
+
+    if(type==end_tag)
+    {
+        if(source[0] != '/') throw xml_except(errc::invalid_token);
+        chop(source);
+    }
+
+    std::string name;
+    if(!parse_name(source, name)) throw xml_except(errc::syntax);
+
+    if(type==end_tag)
+    {
+        if(name!=_M_name) throw xml_except(errc::tag_mismatch);
+    }
+    else _M_name.assign(name);
+
+    parse_space(source);
+
+    if(type!=end_tag) while(true)
+    {
+        attribute x;
+        x._M_parse(source);
+
+        if(x.empty()) break;
+
+        if(count(x.name())) throw xml_except(errc::duplicate_attribute);
+        insert(std::move(x));
+    }
+
+    if(source[0] == '/')
+    {
+        if(type==end_tag) throw xml_except(errc::invalid_token);
+
+        chop(source);
+        empty= true;
+    }
+    if(source[0] != '>') throw xml_except(errc::unclosed_token);
+    chop(source);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+std::string element::value() const
+{
+    if(complex())
+    {
+        std::string x;
+        for(auto& e: _M_children) x+= e._M_write();
+        return x;
+    }
+    else return _M_value;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+bool element::promote()
+{
+    if(_M_tag.empty() && _M_children.size()==1)
+    {
+        element e= std::move(_M_children[0]);
+
+        _M_tag=      std::move(e._M_tag);
+        _M_value=    std::move(e._M_value);
+        _M_children= std::move(e._M_children);
+
+        return true;
+    }
+    else return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void element::insert(const element& x)
+{
+    if(!complex() && _M_value.size())
+    {
+        _M_children.push_back(element(std::string(), std::move(_M_value)));
+        _M_value.clear();
+    }
+
+    _M_children.push_back(x);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void element::insert(element&& x)
+{
+    if(!complex() && _M_value.size())
+    {
+        _M_children.push_back(element(std::string(), std::move(_M_value)));
+        _M_value.clear();
+    }
+
+    _M_children.push_back(std::move(x));
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+std::string element::_M_write(bool nice, int ix) const
+{
+    if(_M_value.empty() && _M_children.empty())
+        return _M_tag.empty()?
+    std::string(): _M_tag._M_write(tag::empty_tag, nice, ix++);
+
+    std::string value;
+    if(!_M_tag.empty()) value+= _M_tag._M_write(tag::start_tag, nice, ix++);
+
+    if(complex())
+        for(auto ri= _M_children.begin(); ri != _M_children.end(); ++ri)
+            value+= ri->_M_write(nice, ix);
+    else value+= decorate(_M_value, nice, ix);
+
+    if(!_M_tag.empty()) value+= _M_tag._M_write(tag::end_tag, nice, --ix);
+    return value;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void element::_M_parse(std::string& source)
+{
+    while(source.size())
+    {
+        if(size_t pos= source.find('<'))
+        {
+            std::string value(source, 0, pos);
+            source.erase(0, pos);
+            insert(std::move(value));
+        }
+        else if(source[1]=='/') // end tag
+        {
+            bool empty;
+            _M_tag._M_parse(source, tag::end_tag, empty);
+
+            break;
+        }
+        else // start tag
+        {
+            element e;
+
+            bool empty= false;
+            e.tag()._M_parse(source, tag::start_tag, empty);
+
+            if(!empty) e._M_parse(source);
+
+            insert(std::move(e));
+        }
+    }
+    promote();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+}
---
+++ xml/xml.h
@@ -0,0 +1,228 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef XML_H
+#define XML_H
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#include "xml_error.h"
+
+#include <initializer_list>
+#include <vector>
+#include <set>
+#include <string>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+namespace xml
+{
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void validate_name(const std::string&);
+void validate_value(const std::string&);
+void validate_content(const std::string&);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+class attribute
+{
+public:
+    attribute() { }
+
+    explicit attribute(const std::string& name, const std::string& value= std::string()):
+        _M_name(name), _M_value(value)
+    { }
+    explicit attribute(std::string&& name, std::string&& value= std::string()):
+        _M_name(std::move(name)), _M_value(std::move(value))
+    { }
+
+    std::string  name() const { return _M_name; }
+    std::string value() const { return _M_value; }
+
+    bool empty() const { return _M_name.empty() && _M_value.empty(); }
+
+    void validate() const
+    {
+        validate_name(_M_name);
+        validate_value(_M_value);
+    }
+
+private:
+    std::string _M_name, _M_value;
+
+    std::string _M_write() const;
+    void _M_parse(std::string& source);
+
+    friend class tag;
+};
+
+inline bool operator<(const attribute& x, const attribute& y) { return x.name() < y.name(); }
+
+typedef std::set<attribute> attributes;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+class tag
+{
+public:
+    tag() { }
+
+    tag(const std::string& name): _M_name(name) { }
+    tag(std::string&& name): _M_name(std::move(name)) { }
+
+    tag(const std::string& name, std::initializer_list<attribute> attributes):
+        _M_name(name),
+        _M_attributes(attributes)
+    { }
+    tag(std::string&& name, std::initializer_list<attribute> attributes):
+        _M_name(std::move(name)),
+        _M_attributes(attributes)
+    { }
+
+    ////////////////////
+    const std::string name() const { return _M_name; }
+
+    bool empty() const { return _M_name.empty() && _M_attributes.empty(); }
+
+    xml::attributes& attributes() { return _M_attributes; }
+    const xml::attributes& attributes() const { return _M_attributes; }
+
+    const attribute* find(const std::string& name) const
+    {
+        auto ri= _M_attributes.find(attribute(name));
+        return ri!=_M_attributes.end()? &*ri: nullptr;
+    }
+    int count(const std::string& name) const { return _M_attributes.count(attribute(name)); }
+
+    std::string value(const std::string& name) const
+    {
+        if(auto ri= find(name))
+            return ri->value();
+        else throw std::out_of_range("tag::value");
+    }
+
+    ////////////////////
+    bool get(std::string& var, const std::string& name)
+    {
+        if(auto value= find(name))
+        {
+            var= value->value();
+            return true;
+        }
+        else return false;
+    }
+
+    ////////////////////
+    bool insert(const attribute& x) { return _M_attributes.insert(x).second; }
+    bool insert(attribute&& x) { return _M_attributes.insert(std::move(x)).second; }
+    void insert(std::initializer_list<attribute> x) { return _M_attributes.insert(x); }
+
+    void validate() const
+    {
+        validate_name(_M_name);
+        for(auto& ri: _M_attributes) ri.validate();
+    }
+
+private:
+    std::string _M_name;
+    xml::attributes _M_attributes;
+
+    enum tag_type { start_tag, end_tag, empty_tag };
+
+    std::string _M_write(tag_type type, bool nice= false, int ix=0) const;
+    void _M_parse(std::string& source, tag_type type, bool& empty);
+
+    friend class element;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+class element;
+typedef std::vector<element> elements;
+
+class element
+{
+public:
+    element() { }
+
+    explicit element(const std::string& name, const std::string& value= std::string()):
+        _M_tag(name),
+        _M_value(value)
+    { }
+    explicit element(std::string&& name, std::string&& value= std::string()):
+        _M_tag(std::move(name)),
+        _M_value(std::move(value))
+    { }
+
+    element(const std::string& name, std::initializer_list<attribute> attributes, const std::string& value= std::string()):
+        _M_tag(name, attributes),
+        _M_value(value)
+    { }
+    element(std::string&& name, std::initializer_list<attribute> attributes, std::string&& value= std::string()):
+        _M_tag(std::move(name), attributes),
+        _M_value(std::move(value))
+    { }
+
+    ////////////////////
+    std::string name() const { return _M_tag.name(); }
+    std::string value() const;
+
+    xml::tag& tag() { return _M_tag; }
+    const xml::tag& tag() const { return _M_tag; }
+
+    bool empty() const { return _M_tag.empty() && _M_children.empty() && _M_value.empty(); }
+    bool complex() const { return !_M_children.empty(); }
+
+    bool promote();
+
+    ////////////////////
+    bool insert(const attribute& x) { return _M_tag.insert(x); }
+    bool insert(attribute&& x) { return _M_tag.insert(x); }
+    void insert(std::initializer_list<attribute> x) { _M_tag.insert(x); }
+
+    void insert(const std::string& value)
+    {
+        if(!complex())
+            _M_value+= value;
+        else insert(element(std::string(), value));
+    }
+    void insert(std::string&& value)
+    {
+        if(!complex())
+            _M_value+= value;
+        else insert(element(std::string(), std::move(value)));
+    }
+
+    void insert(const element&);
+    void insert(element&&);
+
+    void validate()
+    {
+        _M_tag.validate();
+        if(complex())
+            for(element& e: _M_children) e.validate();
+        else validate_content(_M_value);
+    }
+
+    ////////////////////
+    std::string write(bool nice= false) const { return _M_write(nice, 0); }
+    void parse(const std::string& source)
+    {
+        std::string copy= source;
+        _M_parse(copy);
+    }
+    void parse(std::string&& source)
+    {
+        std::string copy= std::move(source);
+        _M_parse(copy);
+    }
+
+private:
+    xml::tag _M_tag;
+
+    std::string _M_value;
+    elements _M_children;
+
+    std::string _M_write(bool nice= false, int ix=0) const;
+    void _M_parse(std::string& source);
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endif // XML_H
