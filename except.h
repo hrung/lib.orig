4c08674ab555b582025a628aee2f52b106b99de6
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2014-07-20 12:30:59 -0400

    except: ease rethrowing
    Now you can do:
    catch(std::runtime_error& e) {
      throw runtime_error(e)
    }
    
    This will rethrow the original error and add context to it.

--- except.h
+++ except.h
@@ -57,6 +57,7 @@ class exception: public std::exception, public context_error
 {
 public:
     using std::exception::exception;
+    exception(const std::exception& e): std::exception(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -64,6 +65,7 @@ class logic_error: public std::logic_error, public context_error
 {
 public:
     using std::logic_error::logic_error;
+    logic_error(const std::logic_error& e): std::logic_error(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -71,6 +73,7 @@ class domain_error: public std::domain_error, public context_error
 {
 public:
     using std::domain_error::domain_error;
+    domain_error(const std::domain_error& e): std::domain_error(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -78,6 +81,7 @@ class invalid_argument: public std::invalid_argument, public context_error
 {
 public:
     using std::invalid_argument::invalid_argument;
+    invalid_argument(const std::invalid_argument& e): std::invalid_argument(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -85,6 +89,7 @@ class length_error: public std::length_error, public context_error
 {
 public:
     using std::length_error::length_error;
+    length_error(const std::length_error& e): std::length_error(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -92,6 +97,7 @@ class out_of_range: public std::out_of_range, public context_error
 {
 public:
     using std::out_of_range::out_of_range;
+    out_of_range(const std::out_of_range& e): std::out_of_range(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -99,6 +105,7 @@ class runtime_error: public std::runtime_error, public context_error
 {
 public:
     using std::runtime_error::runtime_error;
+    runtime_error(const std::runtime_error& e): std::runtime_error(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -106,6 +113,7 @@ class range_error: public std::range_error, public context_error
 {
 public:
     using std::range_error::range_error;
+    range_error(const std::range_error& e): std::range_error(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -113,6 +121,7 @@ class overflow_error: public std::overflow_error, public context_error
 {
 public:
     using std::overflow_error::overflow_error;
+    overflow_error(const std::overflow_error& e): std::overflow_error(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -120,6 +129,7 @@ class underflow_error: public std::underflow_error, public context_error
 {
 public:
     using std::underflow_error::underflow_error;
+    underflow_error(const std::underflow_error& e): std::underflow_error(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -135,6 +145,8 @@ public:
 
     system_error(int v, const std::error_category& c): std::system_error(v, c) { }
     system_error(int v, const std::error_category& c, const std::string& message): std::system_error(v, c, message) { }
+
+    system_error(const std::system_error& e): std::system_error(e) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
