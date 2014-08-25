9a848a46e735192e4d5b96acfe79d632ff1645cc
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2014-08-24 20:43:02 -0400

    logger: rework
    Make streambuf and stream accessible under sys namespace.
    Provide default logger.

--- logger.cpp
+++ logger.cpp
@@ -1,71 +1,57 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include "logger.h"
-#include <streambuf>
-#include <mutex>
 #include <syslog.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-class syslog_streambuf: public std::streambuf
+namespace sys
 {
-protected:
-    static constexpr level reset= level::info;
-    std::streamsize xsputn(const char* message, std::streamsize n) override
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+std::streamsize streambuf::xsputn(const char* message, std::streamsize n)
+{
+    int pri;
+    switch(current)
     {
-        int pri;
-        switch(current)
-        {
-        case level::emergency:
-            pri= LOG_EMERG;
-            break;
-        case level::alert:
-            pri= LOG_ALERT;
-            break;
-        case level::critical:
-            pri= LOG_CRIT;
-            break;
-        case level::error:
-            pri= LOG_ERR;
-            break;
-        case level::warning:
-            pri= LOG_WARNING;
-            break;
-        case level::notice:
-            pri= LOG_NOTICE;
-            break;
-        case level::info:
-            pri= LOG_INFO;
-            break;
-        case level::debug:
-            pri= LOG_DEBUG;
-            break;
-        }
-        syslog(pri, "%s", message);
-
-        current= reset;
-        return n;
+    case emergency:
+        pri= LOG_EMERG;
+        break;
+    case alert:
+        pri= LOG_ALERT;
+        break;
+    case critical:
+        pri= LOG_CRIT;
+        break;
+    case error:
+        pri= LOG_ERR;
+        break;
+    case warning:
+        pri= LOG_WARNING;
+        break;
+    case notice:
+        pri= LOG_NOTICE;
+        break;
+    case info:
+        pri= LOG_INFO;
+        break;
+    case debug:
+        pri= LOG_DEBUG;
+        break;
     }
+    ::syslog(pri, "%s", message);
 
-    level current= reset;
-    friend std::ostream& operator<<(std::ostream&, level);
-};
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-syslog_streambuf buffer;
+    current= default_level;
+    return n;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-std::ostream& operator<<(std::ostream& stream, level x)
+stream& stream::operator<<(level x)
 {
     buffer.current= x;
-    return stream;
+    return (*this);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-struct call_once
-{
-    call_once()
-    {
-        static std::once_flag once;
-        std::call_once(once, []() { std::clog.rdbuf(&buffer); });
-    }
+stream logger;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 }
-redir;
