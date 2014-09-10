15d204ad3fd848b7b4bc42cdb020971d49d63044
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2014-09-10 12:46:51 -0400

    Fix logging

--- lib/log.h
+++ lib/log.h
@@ -3,7 +3,10 @@
 #define LOG_H
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-#include <iostream>
+#include <ostream>
+#include <streambuf>
+#include <string>
+
 #include <syslog.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -28,27 +31,64 @@ enum level
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-class streambuf: public std::streambuf
+class logbuf_base
 {
 protected:
+    void putchar(char c);
+    void set_level(level x) noexcept { _M_level= x; }
+
+    ~logbuf_base();
+
+private:
     static constexpr level default_level= info;
+    level _M_level= default_level;
 
-    std::streamsize xsputn(const char* message, std::streamsize n) override;
-
-    level current= default_level;
-    friend class stream;
+    static constexpr char endl= '\n';
+    std::string _M_buffer;
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-class stream: public std::ostream
+template< typename CharT, typename Traits= std::char_traits<CharT> >
+class basic_logbuf: public std::basic_streambuf<CharT, Traits>, public logbuf_base
 {
 public:
-    stream(): std::ostream(&buffer) { }
-    stream& operator<<(level);
+    typedef CharT                            char_type;
+    typedef Traits                           traits_type;
+    typedef typename traits_type::int_type   int_type;
+    typedef typename traits_type::pos_type   pos_type;
+    typedef typename traits_type::off_type   off_type;
+    typedef typename traits_type::state_type state_type;
 
 protected:
-    streambuf buffer;
+    int_type overflow(int_type c= traits_type::eof()) override
+    {
+        if(!traits_type::eq_int_type(c, traits_type::eof()))
+            putchar(traits_type::to_char_type(c));
+        return c;
+    }
+
+    template<typename, typename> friend class basic_stream;
 };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template< typename CharT, typename Traits= std::char_traits<CharT> >
+class basic_stream: public std::basic_ostream<CharT, Traits>
+{
+public:
+    basic_stream(): std::ostream(&_M_buffer) { }
+
+    basic_stream& operator<<(level x)
+    {
+        _M_buffer.set_level(x);
+        return (*this);
+    }
+
+protected:
+    basic_logbuf<CharT, Traits> _M_buffer;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+typedef basic_stream<char> stream;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 }
