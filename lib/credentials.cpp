90b95af6649604a59ff440b737d5c2a55cef8226
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2014-09-15 14:56:12 -0400

    credentials: update

--- lib/credentials.cpp
+++ lib/credentials.cpp
@@ -2,6 +2,8 @@
 #include "credentials.h"
 #include "errno_error.h"
 
+#include <memory>
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
@@ -15,36 +17,64 @@ namespace app
 credentials::credentials(const std::string& name)
 {
     errno=0;
-    struct passwd* pwd= getpwnam(name.data());
-    if(pwd == nullptr)
+    get(getpwnam(name.data()));
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+credentials::credentials(app::uid uid)
+{
+    errno=0;
+    get(getpwuid(uid));
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void credentials::get(passwd* pwd)
+{
+    if(pwd)
     {
-        if(errno) throw errno_error();
-        else throw std::runtime_error("No entry for "+ name);
+        _M_username= pwd->pw_name;
+        _M_fullname= pwd->pw_gecos;
+        _M_password= pwd->pw_passwd;
+
+        _M_uid= pwd->pw_uid;
+        _M_gid= pwd->pw_gid;
+
+        _M_home= pwd->pw_dir;
+        _M_shell= pwd->pw_shell;
+        if(_M_shell.empty())
+        {
+            setusershell();
+            _M_shell= getusershell();
+            endusershell();
+        }
+
+        int num=0;
+        getgrouplist(pwd->pw_name, pwd->pw_gid, nullptr, &num);
+
+        std::unique_ptr<app::gid[]> buffer(new app::gid[num]);
+        getgrouplist(pwd->pw_name, pwd->pw_gid, buffer.get(), &num);
+
+        for(app::gid* gi= buffer.get(); num; ++gi, --num) _M_groups.insert(*gi);
     }
-
-    username= pwd->pw_name;
-    fullname= pwd->pw_gecos;
-    password= pwd->pw_passwd;
-
-    uid= pwd->pw_uid;
-    gid= pwd->pw_gid;
-
-    home= pwd->pw_dir;
-    shell= pwd->pw_shell;
-    if(shell.empty())
-    {
-        setusershell();
-        shell= getusershell();
-        endusershell();
-    }
+    else if(errno==0)
+        throw std::runtime_error("credentials::get: entry not found");
+    else throw errno_error();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void credentials::morph_into()
 {
-    if(initgroups(username.data(), gid)) throw errno_error();
-    if(setgid(gid)) throw errno_error();
-    if(setuid(uid)) throw errno_error();
+    if(_M_groups.size())
+    {
+        std::unique_ptr<app::gid[]> buffer(new app::gid[_M_groups.size()]);
+
+        app::gid* gi= buffer.get();
+        for(app::gid gid: _M_groups) *gi++= gid;
+
+        if(setgroups(_M_groups.size(), buffer.get())) throw errno_error();
+    }
+    if(setgid(_M_gid)) throw errno_error();
+    if(setuid(_M_uid)) throw errno_error();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
--- lib/credentials.h
+++ lib/credentials.h
@@ -4,28 +4,65 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include <string>
+#include <set>
 #include <sys/types.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+struct passwd;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 namespace app
 {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-struct credentials
+typedef uid_t uid;
+typedef gid_t gid;
+typedef std::set<gid> groups;
+
+constexpr uid invalid_uid= -1;
+constexpr gid invalid_gid= -1;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+class credentials
 {
-    credentials(const std::string& name);
+public:
+    credentials() = default;
+    credentials(const credentials&) = default;
+    credentials(credentials&&) = default;
+
+    credentials& operator=(const credentials&) = default;
+    credentials& operator=(credentials&&) = default;
+
+    explicit credentials(app::uid);
+    explicit credentials(const std::string& name);
+
+    const std::string& username() const noexcept { return _M_username; }
+    const std::string& fullname() const noexcept { return _M_fullname; }
+    const std::string& password() const noexcept { return _M_password; }
+
+    app::uid uid() const noexcept { return _M_uid; }
+    app::uid gid() const noexcept { return _M_uid; }
+
+    const std::string& home() const noexcept { return _M_home; }
+    const std::string& shell() const noexcept { return _M_shell; }
+    const app::groups& groups() const noexcept { return _M_groups; }
 
     void morph_into();
 
-    std::string username;
-    std::string fullname;
-    std::string password;
+private:
+    std::string _M_username;
+    std::string _M_fullname;
+    std::string _M_password;
 
-    uid_t uid;
-    gid_t gid;
+    app::uid _M_uid= invalid_uid;
+    app::gid _M_gid= invalid_gid;
 
-    std::string home;
-    std::string shell;
+    std::string _M_home;
+    std::string _M_shell;
+
+    app::groups _M_groups;
+
+    void get(passwd*);
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
