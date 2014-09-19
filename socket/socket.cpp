2d31d84c939ed23033560014f96dbe1925b47293
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2014-09-19 18:20:55 -0400

    socket: strip down
    Instead of shoving all functions for different types of sockets into one
    class. We split it. This class moves into the app namespace and becomes
    the base for net::socket and unix::socket.

--- socket/socket.cpp
+++ socket/socket.cpp
@@ -10,78 +10,21 @@
 #include "errno_error.h"
 
 #include <memory>
+#include <ctime>
 
 #include <unistd.h>
 #include <fcntl.h>
+
 #include <sys/select.h>
-#include <memory.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-namespace net
+namespace app
 {
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-socket::socket(net::family family, net::type type)
-{
-    _M_family= family;
-    _M_fd= ::socket(static_cast<int>(_M_family), static_cast<int>(type), 0);
-    if(_M_fd == invalid_desc) throw errno_error();
-
-    if(_M_family==family::net)
-    {
-        int value=1;
-        if(setsockopt(_M_fd, SOL_SOCKET, SO_REUSEADDR, &value, sizeof(value))) throw errno_error();
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-socket::~socket()
-{
-    if(_M_fd != invalid_desc) ::close(_M_fd);
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-sockaddr_un socket::from(const std::string& path)
-{
-    sockaddr_un addr;
-
-    addr.sun_family= AF_UNIX;
-    size_t count= path.copy(addr.sun_path, sizeof(addr.sun_path)-1);
-    addr.sun_path[count]=0;
-
-    return addr;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-sockaddr_in socket::from(ipv4::address address, net::port port)
-{
-    sockaddr_in addr;
-
-    addr.sin_family= AF_INET;
-    addr.sin_addr= address._M_addr;
-    addr.sin_port= htons(port);
-
-    return addr;
-}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void socket::bind(sockaddr* addr, socklen_t addr_len)
 {
     if(::bind(_M_fd, addr, addr_len)) throw errno_error();
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::bind(const std::string& path)
-{
-    sockaddr_un addr= from(path);
-    bind((sockaddr*)&addr, sizeof(addr));
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::bind(ipv4::address address, net::port port)
-{
-    sockaddr_in addr= from(address, port);
-    bind((sockaddr*)&addr, sizeof(addr));
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -91,27 +34,13 @@ void socket::connect(sockaddr* addr, socklen_t addr_len)
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::connect(const std::string& path)
-{
-    sockaddr_un addr= from(path);
-    connect((sockaddr*)&addr, sizeof(addr));
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::connect(ipv4::address address, net::port port)
-{
-    sockaddr_in addr= from(address, port);
-    connect((sockaddr*)&addr, sizeof(addr));
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
 void socket::listen(int max)
 {
     if(::listen(_M_fd, max)) throw errno_error();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::accept(net::socket& socket)
+void socket::accept(app::socket& socket)
 {
     socket._M_fd= ::accept(_M_fd, 0, 0);
     if(socket._M_fd <= 0) throw errno_error();
@@ -132,81 +61,18 @@ void socket::set_non_blocking(bool value)
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-bool socket::can_recv(timeval* tv)
+bool socket::can_recv(std::chrono::seconds s, std::chrono::nanoseconds n)
 {
+    timespec time= { static_cast<std::time_t>(s.count()), static_cast<long>(n.count()) };
+
     fd_set fds;
     FD_ZERO(&fds);
     FD_SET(_M_fd, &fds);
 
-    int cnt= select(_M_fd+1, &fds, 0, 0, tv);
-    if(cnt == -1) throw errno_error();
+    int count= pselect(_M_fd+1, &fds, nullptr, nullptr, &time, nullptr);
+    if(count == -1) throw errno_error();
 
-    return cnt;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::set_multicast_loop(bool value)
-{
-    unsigned char opt= (value? 1: 0);
-    if(setsockopt(_M_fd,
-        IPPROTO_IP,
-        IP_MULTICAST_LOOP,
-        &opt,
-        sizeof(opt)))
-    throw errno_error();
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::set_multicast_ttl(unsigned char value)
-{
-    if(setsockopt(_M_fd,
-        IPPROTO_IP,
-        IP_MULTICAST_LOOP,
-        &value,
-        sizeof(value)))
-    throw errno_error();
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::set_multicast_all(bool value)
-{
-    unsigned char opt= (value? 1: 0);
-    if(setsockopt(_M_fd,
-        IPPROTO_IP,
-        IP_MULTICAST_ALL,
-        &opt,
-        sizeof(opt)))
-    throw errno_error();
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::add_membership(ipv4::address group)
-{
-    ip_mreq imr;
-    imr.imr_multiaddr= group._M_addr;
-    imr.imr_interface= ipv4::address::any._M_addr;
-
-    if(setsockopt(_M_fd,
-        IPPROTO_IP,
-        IP_ADD_MEMBERSHIP,
-        &imr,
-        sizeof(imr)))
-    throw errno_error();
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void socket::drop_membership(ipv4::address group)
-{
-    ip_mreq imr;
-    imr.imr_multiaddr= group._M_addr;
-    imr.imr_interface= ipv4::address::any._M_addr;
-
-    if(setsockopt(_M_fd,
-        IPPROTO_IP,
-        IP_DROP_MEMBERSHIP,
-        &imr,
-        sizeof(imr)))
-    throw errno_error();
+    return count;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -223,7 +89,7 @@ ssize_t socket::send(const void* buffer, size_t n, bool wait)
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-ssize_t socket::sendto(sockaddr* addr, socklen_t addr_len, const void* buffer, size_t n, bool wait)
+ssize_t socket::send_to(sockaddr* addr, socklen_t addr_len, const void* buffer, size_t n, bool wait)
 {
     ssize_t count= ::sendto(_M_fd, buffer, n, wait? 0: MSG_DONTWAIT, addr, addr_len);
     if(count == -1)
@@ -233,20 +99,6 @@ ssize_t socket::sendto(sockaddr* addr, socklen_t addr_len, const void* buffer, s
         else throw errno_error();
     }
     return count;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-ssize_t socket::sendto(const std::string& path, const void* buffer, size_t n, bool wait)
-{
-    sockaddr_un addr(from(path));
-    return sendto((sockaddr*)&addr, sizeof(addr), buffer, n, wait);
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-ssize_t socket::sendto(ipv4::address address, net::port port, const void* buffer, size_t n, bool wait)
-{
-    sockaddr_in addr= from(address, port);
-    return sendto((sockaddr*)&addr, sizeof(addr), buffer, n, wait);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -275,7 +127,7 @@ ssize_t socket::recv(void* buffer, size_t max, bool wait)
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-ssize_t socket::recvfrom(sockaddr* addr, socklen_t& addr_len, void* buffer, size_t n, bool wait)
+ssize_t socket::recv_from(sockaddr* addr, socklen_t& addr_len, void* buffer, size_t n, bool wait)
 {
     ssize_t count= ::recvfrom(_M_fd, buffer, n, wait? 0: MSG_DONTWAIT, addr, &addr_len);
     if(count == -1)
@@ -284,62 +136,6 @@ ssize_t socket::recvfrom(sockaddr* addr, socklen_t& addr_len, void* buffer, size
             return 0;
         else throw errno_error();
     }
-    return count;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-ssize_t socket::recvfrom(std::string& path, std::string& string, size_t max, bool wait)
-{
-    std::unique_ptr<char[]> buffer(new char[max+1]);
-
-    ssize_t count= recvfrom(path, buffer.get(), max, wait);
-    buffer[count]=0;
-
-    string.assign(buffer.get(), count);
-    return count;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-ssize_t socket::recvfrom(std::string& path, void* buffer, size_t n, bool wait)
-{
-    sockaddr_un addr;
-    socklen_t addr_len= sizeof(addr);
-    memset(&addr, 0, addr_len);
-
-    ssize_t count= recvfrom((sockaddr*)&addr, addr_len, buffer, n, wait);
-
-    if(net::family(addr.sun_family) == net::family::local) path.assign(addr.sun_path);
-
-    return count;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-ssize_t socket::recvfrom(ipv4::address& address, net::port& port, std::string& string, size_t max, bool wait)
-{
-    std::unique_ptr<char[]> buffer(new char[max+1]);
-
-    ssize_t count= recvfrom(address, port, buffer.get(), max, wait);
-    buffer[count]=0;
-
-    string.assign(buffer.get(), count);
-    return count;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-ssize_t socket::recvfrom(ipv4::address& address, net::port& port, void* buffer, size_t n, bool wait)
-{
-    sockaddr_in addr;
-    socklen_t addr_len= sizeof(addr);
-    memset(&addr, 0, addr_len);
-
-    ssize_t count= recvfrom((sockaddr*)&addr, addr_len, buffer, n, wait);
-
-    if(net::family(addr.sin_family) == net::family::net)
-    {
-        address._M_addr= addr.sin_addr;
-        port= ntohs(addr.sin_port);
-    }
-
     return count;
 }
 
--- socket/socket.h
+++ socket/socket.h
@@ -10,38 +10,22 @@
 #define SOCKET_H
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-#include "address.h"
-
+#include <chrono>
 #include <string>
-#include <ctime>
 
 #include <sys/socket.h>
-#include <sys/un.h>
-#include <netinet/in.h>
+#include <sys/types.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-namespace net
+struct sockaddr;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+namespace app
 {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-typedef in_port_t port;
-
 typedef int desc;
 constexpr desc invalid_desc= -1;
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-enum class type
-{
-    stream= SOCK_STREAM,
-    datagram= SOCK_DGRAM
-};
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-enum class family
-{
-    local= AF_UNIX,
-    net= AF_INET
-};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 class socket
@@ -50,9 +34,7 @@ public:
     socket() noexcept = default;
     socket(const socket&) = delete;
     socket(socket&& x) noexcept { swap(x); }
-
-    socket(net::family, net::type);
-    virtual ~socket();
+    virtual ~socket() = default;
 
     socket& operator=(const socket&) = delete;
     socket& operator=(socket&& x) noexcept
@@ -63,67 +45,41 @@ public:
 
     void swap(socket& x) noexcept
     {
-        std::swap(_M_family, x._M_family);
         std::swap(_M_fd, x._M_fd);
     }
-
-    void bind(const std::string& path);
-    void bind(ipv4::address address, net::port port);
-    void bind(net::port port) { bind(ipv4::address::any, port); }
-
-    void connect(const std::string& path);
-    void connect(ipv4::address address, net::port port);
 
     void listen(int max= 128);
     void accept(socket& socket);
 
     void set_non_blocking(bool);
-    bool can_recv(int wait_usec=0) { timeval tv={0, wait_usec}; return can_recv((wait_usec<0)? 0: &tv); }
-    bool can_recv(timeval* tv);
 
-    void set_multicast_loop(bool);
-    void set_multicast_ttl(unsigned char);
-    void set_multicast_all(bool);
-
-    void add_membership(ipv4::address group);
-    void drop_membership(ipv4::address group);
+    template<typename Rep, typename Period>
+    bool can_recv(const std::chrono::duration<Rep, Period>& x)
+    {
+        std::chrono::seconds s= std::chrono::duration_cast<std::chrono::seconds>(x);
+        std::chrono::nanoseconds n= std::chrono::duration_cast<std::chrono::nanoseconds>(x - s);
+        return can_recv(s, n);
+    }
 
     ssize_t send(const std::string& string, bool wait= true)
         { return send(string.data(), string.size(), wait); }
     ssize_t send(const void* buffer, size_t n, bool wait= true);
 
-    ssize_t sendto(const std::string& path, const std::string& string, bool wait= true)
-        { return sendto(path, string.data(), string.size(), wait); }
-    ssize_t sendto(const std::string& path, const void* buffer, size_t n, bool wait= true);
-
-    ssize_t sendto(ipv4::address address, net::port port, const std::string& string, bool wait= true)
-        { return sendto(address, port, string.data(), string.size(), wait); }
-    ssize_t sendto(ipv4::address address, net::port port, const void* buffer, size_t n, bool wait= true);
-
     ssize_t recv(std::string& string, size_t max, bool wait= true);
     ssize_t recv(void* buffer, size_t max, bool wait= true);
 
-    ssize_t recvfrom(std::string& path, std::string& string, size_t max, bool wait= true);
-    ssize_t recvfrom(std::string& path, void* buffer, size_t n, bool wait= true);
-
-    ssize_t recvfrom(ipv4::address& address, net::port& port, std::string& string, size_t max, bool wait= true);
-    ssize_t recvfrom(ipv4::address& address, net::port& port, void* buffer, size_t n, bool wait= true);
-
-    net::family family() const { return _M_family; }
-    net::desc desc() const { return _M_fd; }
+    app::desc desc() const { return _M_fd; }
 
 protected:
-    net::family _M_family;
-    net::desc _M_fd= invalid_desc;
+    app::desc _M_fd= invalid_desc;
 
-    sockaddr_un from(const std::string&);
-    sockaddr_in from(ipv4::address, net::port);
+    bool can_recv(std::chrono::seconds, std::chrono::nanoseconds);
 
     void bind(sockaddr* addr, socklen_t addr_len);
     void connect(sockaddr* addr, socklen_t addr_len);
 
-    ssize_t sendto(sockaddr* addr, socklen_t addr_len, const void* buffer, size_t n, bool wait= true);
-    ssize_t recvfrom(sockaddr* addr, socklen_t& addr_len, void* buffer, size_t n, bool wait= true);
+    ssize_t send_to(sockaddr* addr, socklen_t addr_len, const void* buffer, size_t n, bool wait= true);
+    ssize_t recv_from(sockaddr* addr, socklen_t& addr_len, void* buffer, size_t n, bool wait= true);
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
