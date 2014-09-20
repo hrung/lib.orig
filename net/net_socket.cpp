c51e6db1f6a3660e687b71d86ad093c81a14564a
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2014-09-19 21:02:44 -0400

    net::socket: fix headers

--- net/net_socket.cpp
+++ net/net_socket.cpp
@@ -12,9 +12,9 @@
 #include <memory>
 #include <cstring>
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 namespace net
